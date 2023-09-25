0ae5cde9a8e6274916bdaf738329d630e53f39e4
Roman Nikitin <nikrom2012@me.com>
2023-09-25 11:15:44 +0300

    Setup frontend linting

--- front/.eslintrc.cjs
+++ front/.eslintrc.cjs
@@ -3,8 +3,11 @@ module.exports = {
     env: {browser: true, es2020: true},
     extends: [
         'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/strict-type-checked',
+        'plugin:@typescript-eslint/stylistic-type-checked',
         'plugin:react-hooks/recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
     ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
@@ -14,5 +17,11 @@ module.exports = {
             'warn',
             {allowConstantExport: true},
         ],
+    },
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: ['./tsconfig.json', './tsconfig.node.json'],
+        tsconfigRootDir: __dirname,
     },
 }
