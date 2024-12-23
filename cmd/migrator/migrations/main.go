38e17e62309c0dd4df8d0f4dc6a26303798eb277
Roman Nikitin <nikrom2012@me.com>
2024-12-23 21:32:33 +0300

    Lint

--- cmd/migrator/migrations/main.go
+++ cmd/migrator/migrations/main.go
@@ -11,6 +11,7 @@ var Migrations = migrate.NewMigrations()
 //go:embed *.sql
 var sqlMigrations embed.FS
 
+//nolint:gochecknoinits // Migrations should be initialized in init functions.
 func init() {
 	if err := Migrations.Discover(sqlMigrations); err != nil {
 		panic(err)
