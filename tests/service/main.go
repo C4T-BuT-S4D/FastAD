d5a2c039386b6345b974e67ca1272eb6bb891eb8
Roman Nikitin <nikrom2012@me.com>
2024-12-26 13:25:14 +0300

    Add testing service with checker

---
+++ tests/service/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	mu := sync.Mutex{}
+	flags := map[string]string{}
+	e.GET("/ping/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "pong")
+	})
+
+	e.POST("/put/", func(c echo.Context) error {
+		var req struct {
+			FlagID string `json:"id"`
+			Vuln   string `json:"vuln"`
+			Flag   string `json:"flag"`
+		}
+		if err := c.Bind(&req); err != nil {
+			return c.String(http.StatusBadRequest, "bad request")
+		}
+
+		mu.Lock()
+		defer mu.Unlock()
+		flags[fmt.Sprintf("%s:%s", req.FlagID, req.Vuln)] = req.Flag
+
+		return c.NoContent(http.StatusOK)
+	})
+
+	e.GET("/get/", func(c echo.Context) error {
+		var req struct {
+			FlagID string `query:"id"`
+			Vuln   string `query:"vuln"`
+		}
+		if err := c.Bind(&req); err != nil {
+			return c.String(http.StatusBadRequest, "bad request")
+		}
+
+		mu.Lock()
+		defer mu.Unlock()
+
+		flag, ok := flags[fmt.Sprintf("%s:%s", req.FlagID, req.Vuln)]
+		if !ok {
+			return c.String(http.StatusNotFound, "not found")
+		}
+
+		return c.JSON(http.StatusOK, map[string]string{"flag": flag})
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
