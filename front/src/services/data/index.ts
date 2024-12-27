97c0cc20b9a106a08728696c69752b9941d22107
Roman Nikitin <nikrom2012@me.com>
2024-12-27 13:59:50 +0300

    Add initial frontend

--- front/src/services/data/index.ts
+++
@@ -1,8 +0,0 @@
-import {createChannel, createClient} from "nice-grpc-web";
-import {TeamsServiceClient, TeamsServiceDefinition} from "../../proto/data/teams/teams_service.ts";
-
-const dataAPIAddress = 'http://localhost:8080'
-
-const channel = createChannel(dataAPIAddress);
-
-export const teamsService: TeamsServiceClient = createClient(TeamsServiceDefinition, channel);
